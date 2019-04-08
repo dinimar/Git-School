145a4b22119e47efc3b3c363edb3d1159c1eef5f
Dinir Imameev <bspncelwm@gmail.com>
2019-04-08 21:21:54 +0300

    feat(views): add /assign-ht page

--- src/main/java/ru/kpfu/itis/gitschool/controllers/UserController.java
+++ src/main/java/ru/kpfu/itis/gitschool/controllers/UserController.java
@@ -1,13 +1,19 @@
 package ru.kpfu.itis.gitschool.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-    @RequestMapping("sign-up")
+    @RequestMapping("/sign-up")
     public String addUser(ModelMap map) {
-        return "home";
+        return "sign-up";
+    }
+
+    @RequestMapping("/assign-ht")
+    public String assignHomeTask(ModelMap map) {
+        return "assign-ht";
     }
 }
